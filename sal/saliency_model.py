82f307070cfa9da156df85317943736cf3034f54
hadimz <hadimoazen@ymail.com>
2022-07-20 14:10:30 +0430

    Update saliency_model.py

--- sal/saliency_model.py
+++ sal/saliency_model.py
@@ -162,8 +162,7 @@ class SaliencyLoss:
         area_loss = calc_area_loss(_masks, self.area_loss_power)
         smoothness_loss = calc_smoothness_loss(_masks)
         
-        sigmoid_loss = torch.mean(torch.sigmoid(100*_masks.clone()))
-        total_loss = sigmoid_loss + destroyer_loss + self.area_loss_coef*area_loss + self.smoothness_loss_coef*smoothness_loss + self.preserver_loss_coef*preserver_loss
+        total_loss = destroyer_loss + self.area_loss_coef*area_loss + self.smoothness_loss_coef*smoothness_loss + self.preserver_loss_coef*preserver_loss
 
         if pt_store is not None:
             # add variables to the pt_store
