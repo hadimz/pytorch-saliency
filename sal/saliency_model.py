a0e9abed4cfe52ffb4123a34c146adecbf09fe92
hadimz <hadimoazen@ymail.com>
2022-07-20 12:57:58 +0430

    reverted back to original

--- sal/saliency_model.py
+++ sal/saliency_model.py
@@ -138,7 +138,7 @@ class SaliencyLoss:
         self.smoothness_loss_coef = smoothness_loss_coef
         self.preserver_loss_coef = preserver_loss_coef
         self.num_classes = num_classes
-        self.area_loss_power =area_loss_power
+        self.area_loss_power = area_loss_power
         self.preserver_confidence = preserver_confidence
         self.destroyer_confidence = destroyer_confidence
         self.apply_mask_kwargs = apply_mask_kwargs
@@ -161,8 +161,9 @@ class SaliencyLoss:
         destroyer_loss = cw_loss(destroyed_logits, _one_hot_targets, targeted=_is_real_target == 0, t_conf=1., nt_conf=self.destroyer_confidence)
         area_loss = calc_area_loss(_masks, self.area_loss_power)
         smoothness_loss = calc_smoothness_loss(_masks)
-
-        total_loss = destroyer_loss + self.area_loss_coef*area_loss + self.smoothness_loss_coef*smoothness_loss + self.preserver_loss_coef*preserver_loss
+        
+        sigmoid_loss = torch.mean(torch.sigmoid(100*_masks.clone()))
+        total_loss = sigmoid_loss + destroyer_loss + self.area_loss_coef*area_loss + self.smoothness_loss_coef*smoothness_loss + self.preserver_loss_coef*preserver_loss
 
         if pt_store is not None:
             # add variables to the pt_store
