ff82c17cf69251b16633b1b2c298f9a198594030
hadimz <hadimoazen@ymail.com>
2022-09-19 16:39:53 -0400

    Update saliency_model.py

--- sal/saliency_model.py
+++ sal/saliency_model.py
@@ -180,7 +180,9 @@ class SaliencyLoss:
         if _masks2 is not None:
             if _masks2.size()[-2:] != _images.size()[-2:]:
                 _masks2 = F.upsample(_masks2, (_images.size(2), _images.size(3)), mode='bilinear')
-            fidelity_loss = torch.mean(torch.abs(_masks-_masks2))
+            # fidelity_loss = torch.mean(torch.abs(_masks-_masks2))
+            fidelity_loss = torch.mean(torch.min(_masks, _masks2) / torch.max(_masks, _masks2))
+
         
         # if _masks3 is not None:
         #     compactness_loss = torch.mean(_masks3)
